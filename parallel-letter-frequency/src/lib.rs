ea459b36637644e49e0523f179e4b6f0f01ea669
esprit <ezprit@gmail.com>
2023-09-21 20:55:00 +0300

    parallel-letter-frequency: simpified and optimized solution

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,52 +1,39 @@
-use std::{
-    collections::HashMap,
-    str::FromStr,
-    sync::{mpsc, Arc, Mutex},
-    thread,
-};
+use std::{collections::HashMap, thread, sync::mpsc};
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    assert!(worker_count > 0);
+    let mut result: HashMap<char, usize> = HashMap::with_capacity(26);
 
-    let mut result: HashMap<char, usize> = HashMap::new();
+    let input = input.join("").chars().collect::<Vec<char>>();
     if input.is_empty() {
         return result;
     }
 
-    let (pool_tx, pool_rx) = mpsc::channel::<String>();
-    let (res_tx, res_rx) = mpsc::channel();
+    let chunk_size = input.len() / worker_count + 1;
+    let (tx, rx) = mpsc::channel::<HashMap<char, usize>>();
 
-    let pool_rx = Arc::new(Mutex::new(pool_rx));
-    for _ in 1..=worker_count {
-        let pool_rx = pool_rx.clone();
-        let res_tx = res_tx.clone();
+    for str in input.chunks(chunk_size) {
+        let str = str.to_owned();
+        let tx = tx.clone();
         thread::spawn(move || {
-            loop {
-                let str = match pool_rx.lock().unwrap().recv() {
-                    Ok(v) => v,
-                    Err(_) => break,
-                };
 
-                let mut cc: HashMap<char, usize> = HashMap::new();
-                for c in str.chars().filter(|c| c.is_alphabetic()) {
-                    *cc.entry(c.to_ascii_lowercase()).or_insert(0) += 1;
-                }
-
-                res_tx.send(cc).unwrap();
+            let mut counter = HashMap::with_capacity(26);
+            let filtered = str
+                .iter()
+                .filter(|ch| ch.is_alphabetic())
+                .map(|ch| ch.to_ascii_lowercase());
+            for char in filtered {
+                *counter.entry(char).or_insert(0) += 1;
             }
+
+            tx.send(counter).unwrap();
         });
     }
 
-    for line in input {
-        pool_tx.send(String::from_str(line).unwrap()).unwrap();
-    }
+    drop(tx);
 
-    drop(pool_tx);
-    drop(res_tx);
-
-    for received in res_rx {
-        for (k, v) in received {
-            *result.entry(k).or_insert(0) += v;
+    for counter in rx {
+        for (&ch, &count) in counter.iter() {
+            *result.entry(ch).or_insert(0) += count;
         }
     }
 
